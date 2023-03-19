6d6650f0f11f0eeed2c247043c4364fa77434bf3
anh89053144136 <anh89053144136@gmail.com>
2023-03-19 16:41:48 +0300

    + Добавил клиентский интерфейс. Added web UI

--- AngularMetroUI/src/main/java/anh/AngularMetroUI/controllers/HomeController.java
+++ AngularMetroUI/src/main/java/anh/AngularMetroUI/controllers/HomeController.java
@@ -1,15 +1,13 @@
 package anh.AngularMetroUI.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.view.RedirectView;
 
 @RestController
 public class HomeController {
 	@GetMapping("/")
-    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-      System.out.println("hello method is starting");
-      
-      return String.format("Hello %s!", name);
+    public RedirectView hello() {
+		return new RedirectView("index.html");
     }
 }
